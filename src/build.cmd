ce92f16a8964a36b540c737759d42778147b73dd
Oleg Shilo <oleg.shilo@gmail.com>
2026-02-26 21:26:41 +1100

    Release v1.0.0.0

--- src/build.cmd
+++ src/build.cmd
@@ -4,4 +4,9 @@ set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Cu
 msbuild explobar.csproj /t:Clean,Build /p:Configuration=Release
 bin\Release\explobar.exe -config-help ..\..\docs\config-help.md
 cd ..
+
+md distro
+del /q distro\*.*
+copy explobar\bin\Release\* distro\
+
 pause 
